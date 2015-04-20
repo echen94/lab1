29db9684e76e6ebbcb680548ad5d488cb301569f
echen94 <chenyiying@ucla.edu>
2015-04-20 16:41:44 -0700

    the TA went over execute_pipe in office hour again, and he has some error checking, and have "_exit " and also setting c->status

--- execute-command.c
+++ execute-command.c
@@ -3,7 +3,6 @@
 #include "command.h"
 #include "command-internals.h"
 
-
 #ifdef __APPLE__
 #include <err.h>
 #define error(args...) errc(args)
@@ -11,20 +10,237 @@
 #include <error.h>
 #endif
 
-/* FIXME: You may need to add #include directives, macro definitions,
-   static function definitions, etc.  */
 
+/* FIXME: You may need to add #include directives, macro definitions,
+ static function definitions, etc.  */
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+//#include<sys/wait.h>
+#include<stdio.h>
+
+void execute_simple(command_t c);
+void execute_sequence(command_t c);
+void execute_and(command_t c);
+void execute_or(command_t c);
+void execute_subshell(command_t c);
+void execute_pipe(command_t c);
 int
 command_status (command_t c)
 {
-  return c->status;
+    return c->status;
 }
 
 void
 execute_command (command_t c, bool time_travel)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
+    /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command execution not yet implemented");
+    if (c==NULL)
+        error (1, 0, "command execution not yet implemented");
+    switch(c->type)
+    {
+        case SIMPLE_COMMAND:
+            execute_simple(c);
+            break;
+        case SEQUENCE_COMMAND:
+            execute_sequence(c);
+            break;
+        case AND_COMMAND:
+            execute_and(c);
+            break;
+        case OR_COMMAND:
+            execute_or(c);
+            break;
+        case SUBSHELL_COMMAND:
+            execute_subshell(c);
+            break;
+        case PIPE_COMMAND:
+            execute_pipe(c);
+            break;
+        default:
+            error (1, 0, "command type illegal");
+            
+    }
+}
+
+void execute_simple(command_t c)// c->word
+{
+    int p=fork();
+    int fd[2];
+    if (c->input != NULL)
+    {
+        fd[0] = open(c->input, O_RDONLY);
+        if (fd[0] < 0)// 0 1 2
+            error(1, 0, "can't open input file");
+    }
+    if (c->output != NULL)
+    {
+        fd[1] = open(c->output, O_WRONLY | O_CREAT | O_TRUNC);
+        if (fd[1] < 0)
+            error(1, 0, "can't open output file");
+    }
+    if(p==0)// child process
+    {
+        if(fd[0]>=0)
+            if(dup2(fd[0],0)<0)
+                error(1,0,"can't read input file");
+        if(fd[1]>=0)
+            if(dup2(fd[1],1) <0)
+                error(1,0,"can't write output file");
+        execvp(c->u.word[0],c->u.word);// 0!!
+        exit(127);// when fails
+        
+    }
+    else{
+        int status;
+        if(waitpid(p,&status,0)<0)// 0: blocking wait
+            error(1,0 ,"waited process error");
+        int exit_status=WEXITSTATUS(status);
+        if(fd[0]>=0)
+            close(fd[0]);
+        if(fd[1]>=0)
+            close(fd[1]);
+        c->status=exit_status;
+    }
+}
+// which order?
+void execute_and(command_t c)// c->command[2]
+{
+    int p=fork();
+    if(p==0){
+        execute_command(c->u.command[0],1);
+    }
+    else{
+        int status;
+        waitpid(p,&status,0);
+        int exit_status=WEXITSTATUS(status);// in lib # include<sys/wait.h> 0-127
+        if(exit_status==0)// successfully run
+        {
+            int q=fork();
+            if(q==0)
+                execute_command(c->u.command[1],1);//execute status of right side
+        }
+        else if (exit_status!=0)
+        {
+            c->status=exit_status;//set exit status to child process
+        }
+        
+    }
+}
+void execute_or(command_t c)// c->command[2]
+{
+    int p=fork();
+    if(p==0){
+        execute_command(c->u.command[0],1);
+    }
+    else{
+        int status;
+        waitpid(p,&status,0);
+        int exit_status=WEXITSTATUS(status);// in lib # include<sys/wait.h> 0-127
+        if(exit_status!=0)// unsuccessfully run
+        {
+            int q=fork();
+            if(q==0)
+                execute_command(c->u.command[1],1);
+        }
+        else if (exit_status==0)
+            c->status=exit_status;
+        
+    }
+}
+void execute_sequence(command_t c) // wait for child to exit
+{
+    int p=fork();
+    if(p==0){
+        execute_command(c->u.command[1],1);
+    }
+    else{
+        int q=fork();
+        if(q==0)
+            execute_command(c->u.command[0],1);
+        else{
+            int status;
+            if(waitpid(p,&status,0)<0)// 0: blocking wait
+                error(1,0 ,"waited process error");
+            int exit_status=WEXITSTATUS(status);
+            c->status=exit_status;// another??!!!
+        }
+    }
+    
+}
+void execute_subshell(command_t c)// c->subshell_command->(word,command[2])
+{
+    int p=fork();
+    int fd[2]={-1,-1};
+    if(c->input!=NULL)
+        fd[0]=open(c->input,O_RDONLY);//READ FILE
+    if(c->output !=NULL)
+        fd[1]=open(c->output, O_CREAT|O_TRUNC|O_WRONLY,0644);//OWNER|GROUP|OTHER
+    
+    if(p==0){
+        if(fd[0]>=0)
+            if(dup2(fd[0],0)<0) error(1,0,"in open input file");
+        if(fd[1] >=0)
+        {
+            if(dup2(fd[1],1)<0) error(1,0,"in open output file");
+        }
+        
+        
+        execute_command(c->u.subshell_command,1);
+   
+    }
+    if(fd[0]>=0) close(fd[0]);
+    if(fd[1]>=0) close(fd[1]);
+    
+}
+void execute_pipe(command_t c)// c->command[2]
+{
+    int fd[2];
+    //from office hour
+    if (pipe(fd)<0)//to replace the pipe(fd) below
+        error(1,0,"pipe fail");
+    pid_t firstpid=fork();
+    
+    if (firstpid<0)
+    {
+        error(1,0,"forkfail");
+    }
+    if (firstpid==0)//execute right side of the pipe
+    {
+        close(fd[1]);
+        if (dup2(fd[0],0)<0)
+            error(1,0,"dup2 fail");//check validity
+        execute_command(c->u.command[1],1);//TA code doesn't have time travel 1, no need this for 1B
+        _exit(c->u.command[1]->status);//propogate exit status to the parent
+    }
+    else //parent execute this code
+    {
+        pid_t secondpid=fork();
+        if (secondpid<0)
+            error(1,0,"fork fail");
+        if (secondpid==0)//child 2 is using fd[1], so close fd[0]
+        {
+            close (fd[0]);
+            if (dup2(fd[1],1)<0)
+                error(1,0,"dup2 fail");
+            execute_command(c->u.command[0],1);
+            _exit(c->u.command[0]->status);
+        }
+        else//parent
+        {
+            close(fd[0]);
+            close(fd[1]);
+            int status;
+            pid_t returnpid=waitpid(-1,&status,0);//
+            if (secondpid==returnpid)
+                waitpid(firstpid,&status,0);//two processes, need to wait another to finish
+            if (firstpid==returnpid)
+                waitpid(secondpid,&status,0);
+            c->status=WEXITSTATUS(status);//extract exit status, least sig 8 bits
+        }
+    }
+    
+
 }
